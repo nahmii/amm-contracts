716555e9be4e439572c27093b59a3f5681a80081
Hein Dauven <heindauven@gmail.com>
2021-08-27 11:44:07 +0200

    Add core and periphery updated repositories.

---
+++ uniswap-v2-periphery/README.md
@@ -0,0 +1,41 @@
+# Uniswap V2
+
+[![Actions Status](https://github.com/Uniswap/uniswap-v2-periphery/workflows/CI/badge.svg)](https://github.com/Uniswap/uniswap-v2-periphery/actions)
+[![npm](https://img.shields.io/npm/v/@uniswap/v2-periphery?style=flat-square)](https://npmjs.com/package/@uniswap/v2-periphery)
+
+In-depth documentation on Uniswap V2 is available at [uniswap.org](https://uniswap.org/docs).
+
+The built contract artifacts can be browsed via [unpkg.com](https://unpkg.com/browse/@uniswap/v2-periphery@latest/).
+
+# Local Development
+
+The following assumes the use of `node@>=10`.
+
+## Install Dependencies
+
+`yarn`
+
+## Compile Contracts
+
+EVM compiler:
+`yarn compile:evm`
+
+NVM compiler:
+`yarn compile:nvm`
+
+## Run Tests
+
+EVM compiler:
+`yarn test:evm`
+
+NVM compiler:
+`yarn test:nvm`
+
+## Deploy
+
+Deploy to the live Nahmii Ropsten network:
+`yarn deploy:nvm`
+
+Alternatively, it is also possible to flatten the smart contracts and deploying them manually.
+To flatten the smart contracts run:
+`npx hardhat flatten contracts/{contract_name}.sol`
---
+++ uniswap-v2-periphery/scripts/deploy.js
@@ -0,0 +1,42 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+    // Get accounts
+    [account] = await ethers.getSigners();
+    deployerAddress = account.address;
+    console.log(`Deploying contracts using ${deployerAddress}`);
+
+    // Wrapped ether address
+    const wETH = "0x4200000000000000000000000000000000000006";
+
+    // Factory address
+    const factory = "0x01a2aB72dd3A49700CFea8b87e9E6ba7Dfb64809";
+
+    // Deploy router
+    const router = await hre.ethers.getContractFactory('UniswapV2Router02');
+    const routerInstance = await router.deploy(factory, wETH);
+    await routerInstance.deployed();
+
+    console.log(`Router02 deployed to: ${routerInstance.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
