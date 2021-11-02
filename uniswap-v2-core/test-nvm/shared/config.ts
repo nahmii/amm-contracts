92f8283fab860469080835fe5d04c4ee98ef9709
Hein <heindauven@gmail.com>
2021-11-02 17:55:47 +0100

    Add environment variable cleaning and defaults to the NVM test configuration.

--- uniswap-v2-core/test-nvm/shared/config.ts
+++ uniswap-v2-core/test-nvm/shared/config.ts
@@ -1,23 +1,24 @@
 import { ethers } from 'ethers'
+import {cleanEnv, str, num} from 'envalid'
 require('dotenv').config();
 
-const config = {
-  l2Url: process.env.L2_URL || 'http://127.0.0.1:8545',
-  pkAccountOne: process.env.PRIVATE_KEY,
-  pkAccountTwo: process.env.PRIVATE_KEY_OTHER,
-  chainId: ethers.BigNumber.from(process.env.CHAIN_ID || 555)
-}
+const config = cleanEnv(process.env, {
+  L2_URL: str({default: 'http://127.0.0.1:8545'}),
+  PRIVATE_KEY: str({default: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'}),
+  PRIVATE_KEY_OTHER: str({default: '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'}),
+  CHAIN_ID: num({default: 555})
+})
 
-const provider = new ethers.providers.JsonRpcProvider(config.l2Url)
+const provider = new ethers.providers.JsonRpcProvider(config.L2_URL)
 provider.getGasPrice = async () => ethers.BigNumber.from(0)
 //@ts-ignore
 provider.getWallets = () => {
   return [
-    new ethers.Wallet(config.pkAccountOne as string).connect(provider),
-    new ethers.Wallet(config.pkAccountTwo as string).connect(provider)
+    new ethers.Wallet(config.PRIVATE_KEY as string).connect(provider),
+    new ethers.Wallet(config.PRIVATE_KEY_OTHER as string).connect(provider)
   ]
 }
 
-const chainId = config.chainId
+const chainId = ethers.BigNumber.from(config.CHAIN_ID)
 
 export { provider, chainId }
