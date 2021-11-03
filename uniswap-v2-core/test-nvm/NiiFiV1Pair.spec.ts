30f1468ebd2b1e46abced60172bc463a91a3e0e8
Jens Ivar Jordre <jensivar@nahmii.io>
2021-11-03 17:08:37 +0100

    Remove overrides

--- uniswap-v2-core/test-nvm/NiiFiV1Pair.spec.ts
+++ uniswap-v2-core/test-nvm/NiiFiV1Pair.spec.ts
@@ -99,7 +99,7 @@ describe('NiiFiV1Pair', () => {
       const [outputAmount, token0Amount, token1Amount, inputAmount] = optimisticTestCase
       await addLiquidity(token0Amount, token1Amount)
       await token0.transfer(pair.address, inputAmount)
-      await expect(pair.swap(outputAmount.add(1), 0, wallet.address, '0x', overrides)).to.be.revertedWith(
+      await expect(pair.swap(outputAmount.add(1), 0, wallet.address, '0x')).to.be.revertedWith(
         'NiiFiV1: K'
       )
       await pair.swap(outputAmount, 0, wallet.address, '0x', overrides)
