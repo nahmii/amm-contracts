4116cacb0079499c20a1ef25f14a56778fa5071e
Jens Ivar Jordre <jensivar@nahmii.io>
2021-11-04 17:09:37 +0100

    Add L1 URL and provider

--- uniswap-v2-core/test-nvm/shared/config.ts
+++ uniswap-v2-core/test-nvm/shared/config.ts
@@ -3,11 +3,14 @@ import {cleanEnv, str, num} from 'envalid'
 require('dotenv').config();
 
 const config = cleanEnv(process.env, {
+  L1_URL: str({default: 'http://127.0.0.1:9545'}),
   L2_URL: str({default: 'http://127.0.0.1:8545'}),
   PRIVATE_KEY: str({default: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'}),
   PRIVATE_KEY_OTHER: str({default: '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'}),
   CHAIN_ID: num({default: 555})
 })
+
+const l1Provider = new ethers.providers.JsonRpcProvider(config.L1_URL)
 
 const provider = new ethers.providers.JsonRpcProvider(config.L2_URL)
 provider.getGasPrice = async () => ethers.BigNumber.from(0)
@@ -21,4 +24,4 @@ provider.getWallets = () => {
 
 const chainId = ethers.BigNumber.from(config.CHAIN_ID)
 
-export { provider, chainId }
+export { l1Provider, provider, chainId }
