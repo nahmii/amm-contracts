aec4d0062ec364fab708381e23a5aacc8f2ff350
Jens Ivar Jordre <jensivar@nahmii.io>
2021-11-04 22:04:45 +0100

    Correct token address retrieval

--- uniswap-v2-core/test-evm/shared/fixtures.ts
+++ uniswap-v2-core/test-evm/shared/fixtures.ts
@@ -37,7 +37,7 @@ export async function pairFixture([wallet]: Wallet[], provider: Web3Provider): P
   const pairAddress = await factory.getPair(tokenA.address, tokenB.address)
   const pair = new Contract(pairAddress, JSON.stringify(NiiFiV1Pair.abi), provider).connect(wallet)
 
-  const token0Address = (await pair.token0()).address
+  const token0Address = await pair.token0()
   const token0 = tokenA.address === token0Address ? tokenA : tokenB
   const token1 = tokenA.address === token0Address ? tokenB : tokenA
 
